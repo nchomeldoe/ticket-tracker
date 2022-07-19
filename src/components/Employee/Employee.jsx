b4d9e73ade256be574094be895ee7e7cac945eeb
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-19 08:49:08 +0100

    changed variable name ticketColour to cardColour

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -2,22 +2,22 @@ import "./Employee.scss";
 import Counter from "../Counter/Counter";
 
 const Employee = ({ id, name, role, displayedCount, setDisplayedCount }) => {
-  let ticketColour;
+  let cardColour;
   let pinColour;
 
   if (displayedCount[id] < 5) {
-    ticketColour = "rgb(97, 215, 97)";
+    cardColour = "rgb(97, 215, 97)";
     pinColour = "rgb(32, 63, 32)";
   } else if (displayedCount[id] < 9) {
-    ticketColour = "rgb(255, 255, 0)";
+    cardColour = "rgb(255, 255, 0)";
     pinColour = "rgb(131, 131, 33)";
   } else {
-    ticketColour = "rgb(208, 59, 59)";
+    cardColour = "rgb(208, 59, 59)";
     pinColour = "rgb(106, 34, 17)";
   }
 
   return (
-    <div className="employee" style={{ backgroundColor: ticketColour }}>
+    <div className="employee" style={{ backgroundColor: cardColour }}>
       <div
         className="employee__pin"
         style={{ backgroundColor: pinColour }}
